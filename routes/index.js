347826b8b46b85fb35bbe44b9f37c1dcd92aca57
mandalidedeepya22 <mandalidedeepya22@gmail.com>
2025-08-22 11:13:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'express' });
+  res.render('index', { title: 'personal portfolio' });
 });
 module.exports = router;
