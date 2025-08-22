3ea27d1007d81cb9c4555481438c584f7d43896d
mandalidedeepya22 <mandalidedeepya22@gmail.com>
2025-08-22 11:39:20 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'personal portfolio' });
+  res.render('index', { 
+    title: 'The Personal Portfolio Website',
+    description: `The Personal Portfolio Website is a web-based platform designed to showcase an individual’s
+    skills, projects, achievements, and contact details. It acts as a digital resume and professional
+    portfolio that highlights one’s personal brand. This project allows recruiters, peers, and potential 
+    collaborators to learn more about the individual in a structured and visually appealing way.`
+  });
 });
+
 module.exports = router;
